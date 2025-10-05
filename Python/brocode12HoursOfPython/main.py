74eb88d3944c374e35732c8aad192386ed15c7e6
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-10-05 22:23:09 +0530

    Day01-variablesSringsTypecasting --Completed

--- Python/brocode12HoursOfPython/main.py
+++ Python/brocode12HoursOfPython/main.py
@@ -37,4 +37,44 @@ print(f"Is all the info checks out ? : {isThisInfoTrue}")
 ####################################### NEW CONCEPT #######################################
 ###########################################################################################
 
-# Typecasting 
+# Typecasting = The process of converting a variable from one data type to another
+#               str(), int(), float(), bool
+
+name = "Saumitra Kulkarni"
+age = 30
+gpa = 4.987654321
+is_student = True
+# profession = "AI/ML Developer"
+profession = ""
+
+gpa = int(gpa)
+
+
+
+if is_student:
+    age = str(age)
+    age += "1"
+    print(f"{name} is {age} years old and has gpa of {gpa}")
+else :
+    age = float(age)
+    age += 1
+    print(f"{name} is {age} years old; where 'age' is {type(age)}")
+
+
+professionBoolean = bool(profession) 
+
+if professionBoolean : 
+    print (f"Professon is entered : {profession}")
+else : 
+    print ("Profession is not entered")
+
+
+# Typecasting is useful for converting userinputs to different dataTypes
+##########################################################################################
+
+
+
+
+
+
+# Lesson ended at 21:00
