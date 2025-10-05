7b3559fba4f63884b3bc1a5b8de2ec27abf442eb
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-10-05 21:04:57 +0530

    Python-Day01-variablesAndFundamentalDataTypes

---
+++ Python/brocode12HoursOfPython/main.py
@@ -0,0 +1,40 @@
+# This is my first program in Python
+print("I like pizza")
+print("It's really good")
+
+###########################################################################################
+####################################### NEW CONCEPT #######################################
+###########################################################################################
+
+
+# Variables = A Container for a value (string, integer, float, boolean)
+#             A variable beahves as if it was the value it contains
+
+# Strings
+firstName = "Saumitra"
+food = "burger"
+
+# Integers 
+age = 30
+numberOfBurgersSaumitraCanEat = 3 
+
+# float 
+price = 249.50
+
+# Boolean
+isThisInfoTrue = True
+
+# This is on the same lines as Java
+print (firstName + " likes " + food + " alot")
+
+# This is python specific 
+print(f"{firstName} loves to eat {food}")
+print(f"{firstName} is {age} years old and \ncan eat {numberOfBurgersSaumitraCanEat} {food}s in a day")
+print(f"Each {food} is RS. {price} /-")
+print(f"Is all the info checks out ? : {isThisInfoTrue}")
+
+###########################################################################################
+####################################### NEW CONCEPT #######################################
+###########################################################################################
+
+# Typecasting 
